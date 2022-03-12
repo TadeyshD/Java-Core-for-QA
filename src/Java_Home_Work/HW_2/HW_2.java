82ea607775c35a94579edd68a526a3f33d5fbcd2
TadeyshD <verydeadman03@gmail.com>
2022-03-12 15:50:52 +0300

    HW_2 Finall

--- src/Java_Home_Work/HW_2/HW_2.java
+++ src/Java_Home_Work/HW_2/HW_2.java
@@ -1,4 +1,5 @@
 package Java_Home_Work.HW_2;
+
 /** 1. Напишите метод, на вход которого подаётся двумерный строковый массив размером 4х4.
  * При подаче массива другого размера необходимо бросить исключение MyArraySizeException.
  2. Далее метод должен пройтись по всем элементам массива, преобразовать в int и просуммировать.
@@ -7,12 +8,48 @@ package Java_Home_Work.HW_2;
  3. В методе main() вызвать полученный метод, обработать возможные исключения MyArraySizeException и MyArrayDataException и вывести результат расчета. **/
 public class HW_2 {
     public static void main(String[] args) {
-        String[][] arr = {{"3", "2", "9", "1"},{"1", "2", "3", "4"},{"6","5","4","3"},{"1", "9", "2", "8"}};
-        for (int i = 0; i < arr.length; i++){
-            for (int j = 0; j < arr.length; j++) {
-                System.out.print(arr[i][j] + " ");
-            }
+        String[][] arr = new String[][]{
+                {"5", "4", "3", "2"},
+                {"5", "4", "3", "2"},
+                {"5", "4", "3", "2"},
+                {"5", "4", "3", "2"},
 
+        };
+        try {
+            try {
+                int end_calc = size_check(arr);
+                System.out.println(end_calc);
+            } catch (MyArraySizeException e) {
+                System.out.println("Нарушена целостность массива.");
+                e.printStackTrace();
+            }
+        } catch (MyArrayDataException e){
+            System.out.println("Ошибка в значении массива.");
+            e.printStackTrace();
         }
+
+
     }
+
+    public static int size_check(String[][] arr) throws MyArraySizeException, MyArrayDataException {
+        int count = 0;
+        if (arr.length != 4) {
+            throw new MyArraySizeException();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length != 4) {
+                throw new MyArraySizeException();
+            }
+            for (int j = 0; j < arr.length; j++) {
+                try {
+                    count = count + Integer.parseInt(arr[i][j]);
+                }
+                catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i, j);
+                }
+            }
+        }
+        return count;
     }
+}
+
--- src/Java_Home_Work/HW_2/Length_exceeded_exception.java
+++
@@ -1,5 +0,0 @@
-package Java_Home_Work.HW_2;
-
-public class Length_exceeded_exception extends Exception {
-
-}
