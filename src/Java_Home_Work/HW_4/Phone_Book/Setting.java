34751e2b86b8df3c5c3262e78b5516eb5b8bc942
TadeyshD <86590703+TadeyshD@users.noreply.github.com>
2022-04-05 23:38:13 +0300

    Start

---
+++ src/Java_Home_Work/HW_4/Phone_Book/Setting.java
@@ -0,0 +1,12 @@
+package Java_Home_Work.HW_4.Phone_Book;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Setting {
+HashMap<String, ArrayList<String>> phone_book = new HashMap<>();
+
+    public HashMap<String, ArrayList<String>> getPhone_book() {
+        return phone_book;
+    }
+}
